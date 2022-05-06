655be0a8a04a9a4960caa4ca2f23ae0552cb878b
blue-collar-programmer <89601260+blue-collar-programmer@users.noreply.github.com>
2022-05-06 11:36:33 -0700

    Finished mobile responsive design, and functionality. Added a new static contentContainer to the index.html , and a static button and button image. Why? to dynamically clear the content container without removing the button. Added a new event listener to the button element in order to make a api request at will.

--- index.js
+++ index.js
@@ -6,10 +6,25 @@
     as the button content 
         - when fired this makes a new api    
 */
-
+// --Global variables
 const url = "https://api.adviceslip.com/advice";
 const mainContainer = document.getElementById('mainContainer');
+const contentContainer = document.getElementById('contentContainer');
+const button = document.getElementById('btn');
+//Create a 404 page and use in error handling 
 async function getAdviceOnLoad(URL) {
+    try {
+        res = await fetch(URL);
+        data = await res.json();
+        console.log("here is the data =>,", data)
+        return data;
+
+    } catch (e) {
+        console.log('the error', e);
+    }
+}
+
+async function getAdviceOnClick(URL) {
     try {
         res = await fetch(URL);
         data = await res.json();
@@ -26,27 +41,66 @@ window.onload = function () {
         .then((advice) => {
             const div = document.createElement("div");
             div.innerHTML = "";
-            const contentContainer = div.classList.add('contentContainer');
-            mainContainer.appendChild(div);
+            //const contentContainer = div.classList.add('contentContainer');
+            contentContainer.appendChild(div);
             console.log(advice)
             div.innerHTML = `
 
             <div class = flexContainer > 
             <h5 class = adviceId> Advice #${advice.slip.id} <h5>
-            <p>${advice.slip.advice}</p>
-            <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
-            <g fill="none" fill-rule="evenodd">
-            <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/>
-            <g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/>
-            <rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+            <p>"${advice.slip.advice}"</p>
+            <div class = partitionContainer>
+            <div class = leftLine></div>
+            <div class = dotsContainer>
+            <div class = middleDots leftDot></div>
+            <div class = middleDots rightDot></div>
             </div>
-        <button class= button>
-        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
-        </button>
+            <div class = rightLine></div>
+            </div>
+            </div>
         `
-       
-
+                    
         })
 
 }
+
+button.addEventListener('click', ()=>{
+    contentContainer.innerHTML='';
+   /* const button = document.createElement('button');
+    button.classList.add('button');
+    mainContainer.appendChild(button);
+    button.innerHTML = `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>`*/
+    getAdviceOnClick(url)
+    .then((advice) => {
+        const div = document.createElement("div");
+        div.innerHTML = "";
+        //const contentContainer = div.classList.add('contentContainer');
+        contentContainer.appendChild(div);
+        console.log(advice)
+        div.innerHTML = `
+
+        <div class = flexContainer > 
+        <h5 class = adviceId> Advice #${advice.slip.id} <h5>
+        <p>"${advice.slip.advice}"</p>
+        <div class = partitionContainer>
+        <div class = leftLine></div>
+        <div class = dotsContainer>
+        <div class = middleDots leftDot></div>
+        <div class = middleDots rightDot></div>
+        </div>
+        <div class = rightLine></div>
+        </div>
+        </div>
+    `
+    
+                
+    })
+
+})
+
+
+
+/*
+
+*/
